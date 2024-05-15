d133c3c1e8840b04cc91148430f52fdee6870d14
kohlkn <kohlkn2@gmail.com>
2024-05-15 17:46:36 -0500

    first

---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import { Text, View, ScrollView, SafeAreaView } from "react-native";
+import { useState } from 'react';
+import { Stack, useRouter } from 'expo-router';
+
+const Index = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>yur</Text>
+    </View>
+  );
+}
+
+export default Index;
